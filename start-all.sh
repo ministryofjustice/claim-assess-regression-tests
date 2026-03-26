83bd61e92e2457d31e6b0621d5481ed85a7920d3
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-03-26 10:52:42 +0000

    Added hostname mapping to start script

--- start-all.sh
+++ start-all.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Update hostname mappings if not already done
+ENTRY="127.0.0.1 stub-oidc-server"
+HOSTS_FILE="/etc/hosts"
+if ! grep -qxF "$ENTRY" "$HOSTS_FILE"; then
+  echo "$ENTRY" | sudo tee -a "$HOSTS_FILE" > /dev/null
+fi
+
 # Additionally add --build --force-recreate to restart the containers with fresh images
 docker compose -f docker-compose.yml -f docker-compose.local.yml up -d
