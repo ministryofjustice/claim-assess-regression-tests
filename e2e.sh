1708522e1f1c7774149606e529f43967714a6a01
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-07-29 08:17:51 +0100

    Reverting unnecessary diff.

--- e2e.sh
+++ e2e.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 export HEADLESS=false
 export TEST_ENV=local
 
+trap './stop-all.sh' EXIT
+
+./start-all.sh
+
 if [[ $# -eq 0 ]]; then
   npx cucumber-js --config cucumber.json
 else
